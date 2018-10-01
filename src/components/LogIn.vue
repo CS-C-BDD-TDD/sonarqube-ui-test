8f7cce3150b5fc9ec9e515c47616aeba6a013144
Lynn Lin <lynn.lin@raytheon.com>
2018-10-01 11:01:54 -0400

    DHS-17: Vue code of the login screen

---
+++ src/components/LogIn.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page class="flex flex-center bg-primary" >
+
+    <div class="window-width row justify-center items-start">
+        <q-card class="bg-white">
+          <q-card-media>
+            <img alt="Quasar logo" src="../assets/DHS-logo.jpg">
+          </q-card-media>
+
+          <!--q-card-separator/-->
+
+          <q-card-title></q-card-title>
+          <q-card-main>
+            <form>
+              <q-field>
+                <q-input v-model="text" float-label="User ID" required></q-input>
+              </q-field>
+              <q-field>
+                <q-input v-model="password" type="password" float-label="Password" 
+                required
+                :after="[
+                  {
+                    icon: 'warning',
+                    error: true,
+                    handler () {
+                      // do something...
+                    }
+                  }
+                ]"
+                >
+                </q-input>
+              </q-field>
+            </form>
+          </q-card-main>
+
+          <q-card-actions>
+            <q-btn class="full-width" type="submit" color="primary" label="Sign In" @click="login" />
+          </q-card-actions>
+        </q-card>
+    </div>
+
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
