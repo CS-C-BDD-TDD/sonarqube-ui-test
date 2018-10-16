e7370eb5b96fa4a00e0de7104b6decb34ace61f7
Lynn Lin <llinmd@yahoo.com>
2018-10-16 01:06:50 -0400

    DHS-15: changes to the view

--- src/components/HumanReview.vue
+++ src/components/HumanReview.vue
@@ -1,144 +1,49 @@
 <template>
-  <div id="message">
   <q-table
     title="Pending Messages"
     :data="tableData"
     :columns="columns"
-    row-key="name"
-  />
-
-  </div>
+    row-key="id"
+  >
+  <q-tr slot="body" slot-scope="props" :props="props">
+    <q-td key="id" :props="props">{{ props.row.stixId }}</q-td>
+    <q-td key="date" :props="props">{{ props.row.actionDate }}</q-td>
+    <q-td key="type" :props="props">{{ props.row.objectType }}</q-td>
+    <q-td key="field" :props="props">{{ props.row.field }}</q-td>
+    <q-td key="value" :props="props">
+      {{ props.row.value }}
+      <q-popup-edit v-model="props.row.value">
+        <q-input v-model="props.row.value" />
+    </q-popup-edit>
+    </q-td>
+    <q-td key="status" :props="props">{{ props.row.status }}</q-td>
+    <q-td key="action" :props="props">{{ props.row.actions }}</q-td>
+    <q-td key="g-action" :props="props">{{ props.row.groupAction }}</q-td>
+  </q-tr>
+</q-table>
 </template>
-
 
 <script>
 export default {
   name: 'humanreview',
   data: () => ({
     columns: [
-      {
-        name: 'id',
-        required: true,
-        label: 'Stix Id',
-        align: 'left',
-        field: 'stixId',
-        sortable: true,
-       // classes: 'my-class',
-        style: 'width: 20px'
-      },
-      {
-        name: 'date',
-        required: true,
-        label: 'Action Date',
-        align: 'left',
-        field: 'actionDate',
-        sortable: true,
-      //  classes: 'my-class',
-        style: 'width: 50px'
-      },
-      {
-        name: 'type',
-        required: true,
-        label: 'Object Type',
-        align: 'left',
-        field: 'objectType',
-        sortable: true,
-     //   classes: 'my-class',
-        style: 'width: 50px'
-      },
-      {
-        name: 'field',
-        required: true,
-        label: 'Field',
-        align: 'left',
-        field: 'field',
-        sortable: true,
-    //    classes: 'my-class',
-        style: 'width: 50px'
-      },
-      {
-        name: 'value',
-        required: true,
-        label: 'Value',
-        align: 'left',
-        field: 'value',
-        sortable: true,
-    //    classes: 'my-class',
-        style: 'width: 50px'
-      },
-      {
-        name: 'status',
-        required: true,
-        label: 'Status',
-        align: 'left',
-        field: 'status',
-        sortable: true,
-   //     classes: 'my-class',
-        style: 'width: 50px'
-      },
-      {
-        name: 'action',
-        required: true,
-        label: 'Action',
-        align: 'left',
-        field: 'actions',
-        sortable: true,
-    //    classes: 'my-class',
-        style: 'width: 50px'
-      },
-      {
-        name: 'g-action',
-        required: true,
-        label: 'Group Action',
-        align: 'left',
-        field: 'groupAction',
-        sortable: true,
-    //    classes: 'my-class',
-        style: 'width: 50px'
-      },
+      { name: 'id', required: true, label: 'Stix Id', align: 'left', field: 'stixId', sortable: true, style: 'width: 10px' },
+      { name: 'date', label: 'Action Date', align: 'left', field: 'actionDate', sortable: true, style: 'width: 50px' },
+      { name: 'type', label: 'Object Type', align: 'left', field: 'objectType', sortable: true, style: 'width: 50px' },
+      { name: 'field', label: 'Field', align: 'left', field: 'field', sortable: true, style: 'width: 50px' },
+      { name: 'value', label: 'Value', align: 'left', field: 'value', sortable: true, style: 'width: 50px' },
+      { name: 'status', label: 'Status', align: 'left', field: 'status', sortable: true, style: 'width: 50px' },
+      { name: 'action', label: 'Action', align: 'left', field: 'actions', sortable: true, style: 'width: 50px' },
+      { name: 'g-action', label: 'Group Action', align: 'left', field: 'groupAction', sortable: true, style: 'width: 50px' },
     ],
     tableData: [
-      {
-        stixId: '1',
-        actionDate: '09/15/2018',
-        objectType: 'Indicator',
-        field: 'Title',
-        value: 'I contain a SSN',
-        status: 'New',
-        actions: 'Select Action',
-        groupAction: 'Disseminate'
-      },
-      {
-        stixId: '1',
-        actionDate: '09/15/2018',
-        objectType: 'Indicator',
-        field: 'Description',
-        value: 'I contain a name',
-        status: 'New',
-        actions: 'Select Action',
-        groupAction: 'Disseminate'
-      },
-      {
-        stixId: '2',
-        actionDate: '09/18/2018',
-        objectType: 'Indicator',
-        field: 'Title',
-        value: 'I contain a SSN: 123-45-6789',
-        status: 'New',
-        actions: 'Select Action',
-        groupAction: 'Disseminate'
-      },
-      {
-        stixId: '2',
-        actionDate: '09/18/2018',
-        objectType: 'Indicator',
-        field: 'Description',
-        value: '123 Massachusetts Ave, NW, Washington DC',
-        status: 'New',
-        actions: 'Select Action',
-        groupAction: 'Disseminate'
-      },
-    ]
+      { stixId: '1', actionDate: '09/15/2018', objectType: 'Indicator', field: 'Title', value: 'I contain a SSN', status: 'New', actions: 'Select Action', groupAction: 'Disseminate' },
+      { stixId: '1', actionDate: '09/15/2018', objectType: 'Indicator', field: 'Description', value: 'I contain a name', status: 'New', actions: 'Select Action', groupAction: 'Disseminate' },
+      { stixId: '2', actionDate: '09/18/2018', objectType: 'Indicator', field: 'Title', value: 'I contain a SSN: 123-45-6789', status: 'New', actions: 'Select Action', groupAction: 'Disseminate' },
+      { stixId: '2', actionDate: '09/18/2018', objectType: 'Indicator', field: 'Description', value: '123 Massachusetts Ave, NW, Washington DC', status: 'New', actions: 'Select Action', groupAction: 'Disseminate' },
+    ],
+    //separator: 'horizontal'
   })
 }
 </script>
